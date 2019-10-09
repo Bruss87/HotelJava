245561d152417935ca3e92b59af1e6a07bacf498
Karolina Kaczmarska <kaczmarska.kar@gmail.com>
2019-10-09 11:58:53 +0100

    adds Conference Room class' capacity and collectionOfGuests

--- src/main/java/ConferenceRoom.java
+++ src/main/java/ConferenceRoom.java
@@ -1,12 +1,27 @@
+import java.util.ArrayList;
+
 public class ConferenceRoom {
 
     private String name;
+    private int capacity;
+    private ArrayList<Guest>collectionOfGuests;
 
-    public ConferenceRoom(String name){
+    public ConferenceRoom(String name, int capacity) {
         this.name = name;
+        this.capacity = capacity;
+        this.collectionOfGuests = new ArrayList<Guest>();
     }
 
-    public String getName(){
+    public String getName() {
         return this.name;
     }
+
+    public int getCapacity() {
+        return this.capacity;
+    }
+    
+    public int getCollectionOfGuests() {
+        return this.collectionOfGuests.size();
+    }
+
 }
--- src/test/java/ConferenceRoomTest.java
+++ src/test/java/ConferenceRoomTest.java
@@ -9,11 +9,23 @@ public class ConferenceRoomTest {
 
     @Before
     public void before(){
-        conferenceRoom1 = new ConferenceRoom("Atlantic");
+        conferenceRoom1 = new ConferenceRoom("Atlantic", 3);
     }
 
     @Test
     public void hasName(){
         assertEquals("Atlantic", conferenceRoom1.getName());
     }
+
+    @Test
+    public void hasCapacity() {
+        assertEquals(3, conferenceRoom1.getCapacity());
+    }
+
+    @Test
+    public void collectionOfGuestsBeginsAtZero() {
+        assertEquals(0, conferenceRoom1.getCollectionOfGuests());
+
+    }
+
 }
